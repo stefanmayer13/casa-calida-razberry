27726d466f2eec4bd80756f508866bf688f06bb5
Stefan Mayer <stefanmayer13@gmail.com>
2017-04-08 22:31:42 +0200

    Supporting IOT actions and added yarn.lock file

--- src/iot/index.js
+++ src/iot/index.js
@@ -16,6 +16,8 @@ const iotMapping = {
     'casa-calida-temperature': temperatureSensorConverter,
 };
 
+const actuatorMapping = {};
+
 function getIotData(ips, websocket) {
     logger.verbose('Getting data from iot devices');
     return Promise.all(ips.map(ip => {
@@ -61,6 +63,7 @@ function fullIotData(ips, websocket) {
                     logger.error(`Couldn't get iot data from ${ip}. Code ${data.statusCode}`);
                     throw new Error(`${data.statusCode} ${data.error}`);
                 }
+                actuatorMapping[data.body.id] = {ip};
                 return data.body;
             }).catch((e) => {
                 logger.error(e);
@@ -72,6 +75,9 @@ function fullIotData(ips, websocket) {
             const converter = iotMapping[data.type];
             if (converter) {
                 const sensors = converter(data, 'iot', data.id);
+                actuatorMapping[data.id].actuators = sensors
+                    .filter(sensor => sensor.deviceType === 'actuator')
+                    .map(actuator => actuator.key);
                 return {
                     deviceId: data.id,
                     name: data.name,
@@ -93,7 +99,23 @@ function fullIotData(ips, websocket) {
     });
 }
 
+function action(id, value) {
+    const deviceId = id.substring(4, id.indexOf('-', 4));
+    const ip = actuatorMapping[deviceId].ip
+    return request.get(`http://${ip}/${value}`).then((data) => {
+        if (data.statusCode !== 200) {
+            log.error(`Couldn't send command ${value} to ${id}. Code ${data.statusCode}`);
+            throw new Error(`${data.statusCode} ${data.error}`);
+        }
+        return data.body;
+    });
+}
+
 module.exports = function iot(ips, websocket) {
     setInterval(getIotData.bind(null, ips, websocket), 300000);
-    return fullIotData(ips, websocket);
+    return fullIotData(ips, websocket)
+        .then((data) => {
+            casaCalida.register('iot', {action});
+            return data;
+        });
 };
